21abb6d56d6f4aa998fdf133a9d8793a4ce7610d
Graham Ollis <perl@wdlabs.com>
2013-06-07 15:24:00 -0400

    make it ready for initial release

--- lib/NX/Lock.pm
+++ lib/NX/Lock.pm
@@ -8,6 +8,52 @@ use IO::File;
 # ABSTRACT: OO Lock interface
 # VERSION
 
+=head1 SYNOPSIS
+
+ do {
+   my $lock = NX::Lock->new("/tmp/foo.lock");
+   die 'no lock!' unless defined $lock;
+   # do something which requires the lock
+ };
+ # lock automatically released
+
+=head1 DESCRIPTION
+
+OO interface to flock.  The lock is automatically 
+released when the lock object falls out of scope
+
+=head1 CONSTRUCTOR
+
+=head2 NX::Lock->new( $filename, %options )
+
+On success returns a lock.
+
+Blocks until the lock is obtained if in blocking mode.
+
+Returns undef if in non blocking mode and lock 
+could not be obtained.
+
+Options:
+
+=over 4
+
+=item block
+
+Blocking mode.
+
+=item quit_on_fail
+
+Quit the entire program (NOT die!) if not in
+blocking mode and lock can not be obtained.
+
+=item shared
+
+Obtain a shared lock.
+
+=back
+
+=cut
+
 sub new
 {
   my $obclass = shift;
@@ -50,6 +96,14 @@ sub new
   
   return undef;
 }
+
+=head1 METHODS
+
+=head2 $lock-E<gt>release
+
+Release the lock.
+
+=cut
 
 sub release
 {
